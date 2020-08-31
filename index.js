e07f435ef51ba035ec27ec68311e65a07fef7db6
Luis Pedro Pinheiro <47849666+luispedropinheiro@users.noreply.github.com>
2020-08-31 22:57:33 +0200

    Drink Water Reminder Tweet
    Done

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-//
-//  tweet 'Water Reminder'
-//
-T.post('statuses/update', { status: 'Test: Go get some water !' }, function(err, data, response) {
-  console.log(data)
-})
+let nbReminder = 0;
+var Twit = require('twit');
+
+// Twitter's API
+var T = new Twit({
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+});
+
+SetAnInterval();
+
+function SetAnInterval() {
+  setInterval(postTweet,1000*60*60);
+}
+
+function postTweet()
+{
+  nbReminder += 1;
+  //
+  //  tweet 'Water Reminder'
+  //
+  T.post('statuses/update', { status: "Reminder n°" + nbReminder +" \n🥤 Go get some water bro 🥤 " }, function(err, data, response) {
+    console.log(data);
+  });
+  return "Done";
+}
